b64d505a0d322c2d032bd15454bb566fdd97ad9f
peakyblinder-kim <56843503+peakyblinder-kim@users.noreply.github.com>
2022-11-30 17:16:10 +0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,15 +1,5 @@
-#import pytube library to download the video
 import pytube
-
-#Ask for the url of video
 url = input("Enter video url")
-#we can take path as well, just comment the following line
-#path = input("Enter the path of storage")
-
-#specify the storage path of video
 path="E:"
-
-#magic line to download the video
 pytube.Youtube(url).streams.get_highest_resolution().download(path)
 
-#clcoding.com
